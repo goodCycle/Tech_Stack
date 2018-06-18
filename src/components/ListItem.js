ae8cfa8554e6b69f82043dba9ee968a4af125375
goodCycle <kitiyj526@gmail.com>
2018-06-18 17:55:29 +0900

    Finish the App, exceptthere is a empty item at the end of the list

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+  
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text>{library.description}</Text>
+        </CardSection>
+      );
+    }
+  }
+
+  onPressHandler(id) {
+    if (id == undefined) {
+      return null;
+    } else {
+      this.props.selectLibrary(id);
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library;
 
     return (
       <TouchableWithoutFeedback
-        onPress={() => this.props.selectLibrary(id)}
-      >
+        onPress={() => {
+            this.onPressHandler(id)
+          }}>
         <View>
           <CardSection>
             <Text style={titleStyle}>{title}</Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -30,4 +61,10 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
