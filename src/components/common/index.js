74be0ddfa2b576da4fa091c977d190d04a06cf7d
goodCycle <kitiyj526@gmail.com>
2018-06-18 15:22:58 +0900

    Made a tech list usingreducers

---
+++ src/components/common/index.js
@@ -0,0 +1,8 @@
+// index.js에서 모든 컴포넌트를 사용할 수 있도록 하기 위해.
+
+export * from './Card.js';
+export * from './Button.js';
+export * from './Header.js';
+export * from './Input.js';
+export * from './CardSection.js';
+export * from './Spinner.js';
---
+++ src/reducers/LibraryReducer.js
@@ -0,0 +1,3 @@
+import * as data from './LibraryList.json';
+
+export default () => data;
